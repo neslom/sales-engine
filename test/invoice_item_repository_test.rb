0f897de1d5d5cb98789d458f124ca8ea5cdf5148
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-01-22 08:07:08 -0700

    finished invoice item files

---
+++ test/invoice_item_repository_test.rb
@@ -0,0 +1,61 @@
+require 'minitest/test'
+require 'minitest/pride'
+require_relative '../lib/invoice_item_repository'
+
+class InvoiceItemRepositoryTest < MiniTest::Test
+  attr_reader :invoice_items_repo
+  def setup
+    @invoice_items_repo = InvoiceItemRepository.new('test/support/sample_invoice_items.csv')
+  end
+
+  def test_it_parses_csv_file
+    first = invoice_items_repo.invoice_items.first
+    assert_equal 1, first.id
+    assert_equal 539, first.item_id
+
+    second = invoice_items_repo.invoice_items[1]
+    assert_equal 2, second.id
+    assert_equal 528, second.item_id
+  end
+
+  def test_all_returns_all_invoice_item_instances
+    invoice_item_count = invoice_items_repo.all
+    assert_equal 10, invoice_item_count.size 
+  end
+
+  def test_random_returns_random_invoice_item
+    rand = invoice_items_repo.random
+    rand_collection = []
+    rand_collection << rand
+    assert_equal 1, rand_collection.size
+    assert_includes invoice_items_repo.invoice_items, rand
+  end
+
+  def test_find_by_attribute
+    result = invoice_items_repo.find_by_item_id(535)
+    assert_equal 4, result.id
+
+    result2 = invoice_items_repo.find_by_invoice_id(1)
+    assert_equal 1, result2.id
+
+    result3 = invoice_items_repo.find_by_quantity(4)
+    assert_equal 7, result3.id
+
+    result4 = invoice_items_repo.find_by_unit_price(29973)
+    assert_equal 9, result4.id
+  end
+
+  def test_find_all_by_attribute
+    result = invoice_items_repo.find_all_by_item_id(523)
+    assert_equal 1, result.size
+
+    result2 = invoice_items_repo.find_all_by_invoice_id(1)
+    assert_equal 8, result2.size
+
+    result3 = invoice_items_repo.find_all_by_quantity(4)
+    assert_equal 2, result3.size
+
+    result4 = invoice_items_repo.find_all_by_unit_price(29973)
+    assert_equal 1, result4.size
+  end
+end
